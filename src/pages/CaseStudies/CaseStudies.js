84c9b5ef30528bcda736c0d5d108f41f60c3ceeb
steph-crown <emmanuelstephen024@gmail.com>
2022-07-19 15:16:13 +0100

    Complete navbar

--- src/pages/CaseStudies/CaseStudies.js
+++ src/pages/CaseStudies/CaseStudies.js
@@ -1,9 +1,14 @@
+// Imports
 import React from "react";
+import { Components } from "../../components";
 import { CaseStudiesWrapper } from "./styles";
 
+// Destructure imports
+const { Navbar } = Components;
 const CaseStudies = () => {
   return (
     <CaseStudiesWrapper>
+      <Navbar />
       <h1>CaseStudies</h1>
     </CaseStudiesWrapper>
   );
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,14 @@
+// Imports
 import React from "react";
+import { Components } from "../../components";
 import { ContactWrapper } from "./styles";
 
+// Destructure imports
+const { Navbar } = Components;
 const Contact = () => {
   return (
     <ContactWrapper>
+      <Navbar />
       <h1>Contact</h1>
     </ContactWrapper>
   );
