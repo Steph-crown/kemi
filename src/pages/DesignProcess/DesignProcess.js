07ef9a0d7093ce59bb3f9f50f64697f85eda0580
steph-crown <emmanuelstephen024@gmail.com>
2022-07-19 09:09:12 +0100

    Create design process page

---
+++ src/pages/DesignProcess/DesignProcess.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { DesignProcessWrapper } from "./styles";
+
+const DesignProcess = () => {
+  return (
+    <DesignProcessWrapper>
+      <h1>DesignProcess</h1>
+    </DesignProcessWrapper>
+  );
+};
+
+export default DesignProcess;
