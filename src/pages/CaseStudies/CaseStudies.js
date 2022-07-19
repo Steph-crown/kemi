0cfa5eef77382b2149a1298df7be0c52cd2e3c52
steph-crown <emmanuelstephen024@gmail.com>
2022-07-19 09:08:55 +0100

    Create case studies page

---
+++ src/pages/CaseStudies/CaseStudies.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { CaseStudiesWrapper } from "./styles";
+
+const CaseStudies = () => {
+  return (
+    <CaseStudiesWrapper>
+      <h1>CaseStudies</h1>
+    </CaseStudiesWrapper>
+  );
+};
+
+export default CaseStudies;
