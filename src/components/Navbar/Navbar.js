7a034ab1ce2a481d54003396d55e21a4449cc1e7
steph-crown <emmanuelstephen024@gmail.com>
2022-07-19 09:33:02 +0100

    Create navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavbarWrapper } from "./styles";
+
+const Navbar = () => {
+  return (
+    <NavbarWrapper className="container">
+      <section className="container__sub">
+        <h1>Navbar</h1>
+      </section>
+    </NavbarWrapper>
+  );
+};
+
+export default Navbar;
