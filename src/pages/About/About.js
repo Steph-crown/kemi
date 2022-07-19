1fb7ac4a23bc9c2d9492ad040f5f345b130862c1
steph-crown <emmanuelstephen024@gmail.com>
2022-07-19 09:05:39 +0100

    Create about page

---
+++ src/pages/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AboutWrapper } from "./styles";
+
+const About = () => {
+  return (
+    <AboutWrapper>
+      <h1>About</h1>
+    </AboutWrapper>
+  );
+};
+
+export default About;
