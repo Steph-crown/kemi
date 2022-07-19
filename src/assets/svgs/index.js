824dcc83c5b15b2abd6cbabd017faf8eec33eea9
steph-crown <emmanuelstephen024@gmail.com>
2022-07-19 13:38:04 +0100

    Add pen icon

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,4 +1,5 @@
 import { ReactComponent as MenuIcon } from "./menu.svg";
 import { ReactComponent as Logo } from "./logo.svg";
+import { ReactComponent as PenIcon } from "./pen.svg";
 
-export const svgs = { MenuIcon, Logo };
+export const svgs = { MenuIcon, Logo, PenIcon };
