d720a47a5d57bbd50278cf25b6d069e34c235fe8
steph-crown <emmanuelstephen024@gmail.com>
2022-07-19 08:55:08 +0100

    Add custom font faces

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Yoo</div>;
+  return (
+    <div
+      className="App"
+      style={{
+        fontFamily: "Made, serif",
+        fontWeight: "700",
+        fontSize: "40px",
+      }}
+    >
+      Yoo
+    </div>
+  );
 }
 
 export default App;
