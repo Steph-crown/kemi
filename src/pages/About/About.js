0bfc36b61c66c242ebe517475c6aef3ed7cb87fd
steph-crown <emmanuelstephen024@gmail.com>
2022-07-23 17:01:30 +0100

    Add contact section to other pages

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,13 +4,14 @@ import { Components } from "../../components";
 import { AboutWrapper } from "./styles";
 
 // Destructure imports
-const { Navbar } = Components;
+const { Navbar, ContactSection } = Components;
 
 const About = () => {
   return (
     <AboutWrapper>
       <Navbar />
       <h1>About</h1>
+      <ContactSection />
     </AboutWrapper>
   );
 };
--- src/pages/CaseStudies/CaseStudies.js
+++ src/pages/CaseStudies/CaseStudies.js
@@ -4,12 +4,13 @@ import { Components } from "../../components";
 import { CaseStudiesWrapper } from "./styles";
 
 // Destructure imports
-const { Navbar } = Components;
+const { Navbar, ContactSection } = Components;
 const CaseStudies = () => {
   return (
     <CaseStudiesWrapper>
       <Navbar />
       <h1>CaseStudies</h1>
+      <ContactSection />
     </CaseStudiesWrapper>
   );
 };
